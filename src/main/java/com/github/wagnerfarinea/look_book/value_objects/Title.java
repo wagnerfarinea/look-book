f33d2385ef16e1bdb952549a24beafb8de69f266
Wagner Farinea <wagner.farinea@gmail.com>
2022-05-21 17:00:52 -0300

    feat(book): create a simple value object for the book title

---
+++ src/main/java/com/github/wagnerfarinea/look_book/value_objects/Title.java
@@ -0,0 +1,13 @@
+package com.github.wagnerfarinea.look_book.value_objects;
+
+public class Title {
+    private final String text;
+
+    public Title(String title) {
+        this.text = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
