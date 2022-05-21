7900b1b0306c6122a238b05a385b9be130f38e6d
Wagner Farinea <wagner.farinea@gmail.com>
2022-05-21 17:13:47 -0300

    test(book): add null or empty title validation test

--- src/test/java/com/github/wagnerfarinea/look_book/value_objects/TitleTest.java
+++ src/test/java/com/github/wagnerfarinea/look_book/value_objects/TitleTest.java
@@ -1,8 +1,10 @@
 package com.github.wagnerfarinea.look_book.value_objects;
 
+import com.github.wagnerfarinea.look_book.exceptions.RequiredFieldException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class TitleTest {
@@ -15,4 +17,11 @@ class TitleTest {
         Assertions.assertEquals(text, title.getText());
     }
 
+    @ParameterizedTest
+    @DisplayName("Failed  Scenario - An a exception should be thrown when title is null or empty")
+    @ValueSource(strings = {""," "})
+    @NullSource
+    void mustThrowRequiredException(String text) {
+        Assertions.assertThrows(RequiredFieldException.class, () -> new Title(text));
+    }
 }
