c58aab7cd89eae028b3bb1aeb5a746dd96cc41e2
Wagner Farinea <wagner.farinea@gmail.com>
2022-05-21 17:38:24 -0300

    feat(book): add title validation less than 3 characters

--- src/main/java/com/github/wagnerfarinea/look_book/value_objects/Title.java
+++ src/main/java/com/github/wagnerfarinea/look_book/value_objects/Title.java
@@ -1,5 +1,6 @@
 package com.github.wagnerfarinea.look_book.value_objects;
 
+import com.github.wagnerfarinea.look_book.exceptions.InvalidFieldSizeException;
 import com.github.wagnerfarinea.look_book.exceptions.RequiredFieldException;
 import com.github.wagnerfarinea.look_book.utils.StringUtils;
 
@@ -10,6 +11,11 @@ public class Title {
         if (StringUtils.isBlank(title)) {
             throw new RequiredFieldException("Book Title");
         }
+
+        if (title.length() < 3) {
+            throw new InvalidFieldSizeException("Book Title", 3);
+        }
+
         this.text = title;
     }
 
