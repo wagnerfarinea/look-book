9108bd3d36b4fbf99ebd70193f84b8886a1d8881
Wagner Farinea <wagner.farinea@gmail.com>
2022-05-21 17:24:24 -0300

    feat(book): add null or empty title validation

--- src/main/java/com/github/wagnerfarinea/look_book/value_objects/Title.java
+++ src/main/java/com/github/wagnerfarinea/look_book/value_objects/Title.java
@@ -1,9 +1,15 @@
 package com.github.wagnerfarinea.look_book.value_objects;
 
+import com.github.wagnerfarinea.look_book.exceptions.RequiredFieldException;
+import com.github.wagnerfarinea.look_book.utils.StringUtils;
+
 public class Title {
     private final String text;
 
     public Title(String title) {
+        if (StringUtils.isBlank(title)) {
+            throw new RequiredFieldException("Book Title");
+        }
         this.text = title;
     }
 
